208026917cad0abd9bc0db892b11632653460421
Krzysztof Smykla <132474923+Krzysztof-Smykla@users.noreply.github.com>
2025-09-08 18:43:17 +0200

    Add files via upload

---
+++ 2704. To Be or Not To Be.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string|number} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe(val2) {
+            if (val === val2) {
+                return true;
+            } else {
+                throw "Not Equal";
+            }
+        },
+        notToBe(val2) {
+            if (val !== val2) {
+                return true;
+            } else {
+                throw "Equal";
+            }
+        }
+    };
+};
+
+/**
+ * Examples:
+ * expect(5).toBe(5);      // true
+ * expect(5).notToBe(5);   // throws "Equal"
+ */
